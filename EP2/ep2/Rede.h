6e81625a2ebddf7be7b484efee0170b959d94ae7
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2020-12-03 12:58:08 -0300

    Atualização 03-12
    Adição da classe rede;
    Modificação no arquivo do projeto;

---
+++ EP2/ep2/Rede.h
@@ -0,0 +1,36 @@
+#ifndef REDE_H
+#define REDE_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+#include "No.h"
+#include "Roteador.h"
+#include "Processo.h"
+#include "Hospedeiro.h"
+
+
+class Rede
+{
+    public:
+        Rede();
+        virtual ~Rede();
+        virtual void adicionar(No* no);
+
+        virtual No* getNo(int endereco);
+        virtual list<Hospedeiro*>* getHospedeiro();
+
+        virtual void passarTempo();
+
+        virtual void imprimir();
+
+    protected:
+
+    private:
+        vector<No*>* nosDaRede;
+        list<Hospedeiro*>* hospedeiroDaRede;
+};
+
+#endif // REDE_H
